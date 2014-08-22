75ff8fcbde5ebb22777d98ce2f52005bffbeae1a
KthProg <kthprog@gmail.com>
2014-08-22 15:48:46 -0400

    Issues nearly complete.

--- AngularWCM/scripts/js/issue.js
+++ AngularWCM/scripts/js/issue.js
@@ -9,15 +9,28 @@
     $scope.getOpenIssues = function () {
         $http.get("/scripts/php/Query.php?Query=GetOpenIssues&ASSOC=true&Params=[]")
         .success(function (resp) {
-            $scope.openIssues = resp;
+            var formatted = $scope.formatResponse(resp);
+            $scope.openIssues = formatted;
         });
     };
 
     $scope.getClosedIssues = function () {
         $http.get("/scripts/php/Query.php?Query=GetClosedIssues&ASSOC=true&Params=[]")
         .success(function (resp) {
-            $scope.closedIssues = resp;
+            var formatted = $scope.formatResponse(resp);
+            $scope.closedIssues = formatted;
         });
+    };
+
+    $scope.formatResponse = function (resp) {
+        for (var i in resp) {
+            for (var k in resp[i]) {
+                if (!isNaN(resp[i][k])) {
+                    resp[i][k] = Number(resp[i][k]);
+                }
+            }
+        }
+        return resp;
     };
 
     $scope.getOpenIssues();
