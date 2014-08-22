bd5d14c6cd8fdc6dc46c480d2c8027a0a12b2224
KthProg <kthprog@gmail.com>
2014-08-22 15:28:38 -0400

    Working on converting issues to AngularJS

---
+++ AngularWCM/scripts/js/issue.js
@@ -0,0 +1,28 @@
+﻿function Issues($scope, $http) {
+
+    $scope.showOpen = true;
+    $scope.showClosed = true;
+
+    $scope.openIssues = [{ Name: "Initializing" }];
+    $scope.closedIssues = [{ Name: "Initializing"}];
+
+    $scope.getOpenIssues = function () {
+        $http.get("/scripts/php/Query.php?Query=GetOpenIssues&ASSOC=true&Params=[]")
+        .success(function (resp) {
+            $scope.openIssues = resp;
+        });
+    };
+
+    $scope.getClosedIssues = function () {
+        $http.get("/scripts/php/Query.php?Query=GetClosedIssues&ASSOC=true&Params=[]")
+        .success(function (resp) {
+            $scope.closedIssues = resp;
+        });
+    };
+
+    $scope.getOpenIssues();
+    $scope.getClosedIssues();
+
+}
+
+app.controller("Issues", Issues);
---
+++ AngularWCM/scripts/js/wcm.js
@@ -0,0 +1,76 @@
+﻿//
+// ANGULAR (FORM) FUNCTIONS! ================================================
+//
+
+var app = angular.module("wcm", []);
+
+//
+// IMAGE FUNCTIONS! =====================================================
+//
+function changeImage(input, imgEl) {
+    var isBlank = (input.val() == "");
+    imgEl.attr("src", (isBlank ? "res/upload.png" : "res/uploadgreen.png"));
+    //console.log("src att changed to " + (isBlank ? "res/upload.png" : "res/uploadgreen.png"));
+}
+
+
+function imgToBase64(imgEl, hiddenEl) {
+
+    var scope = angular.element($("[ng-app='wcm']")).scope();
+    var fileReader = new FileReader();
+
+    fileReader.onload = function (e) {
+        scope.lastImage.URI = e.target.result;
+        scope.$apply();
+    }
+
+    scope.lastImage.name = imgEl.files[0].fileName || imgEl.files[0].name;
+    fileReader.readAsDataURL(imgEl.files[0]);
+}
+
+//
+// SKETCH FUNCTIONS! ====================================================
+//
+
+/*
+This doesn't work correctly if the sketch area has a size set manually
+i.e. in CSS or some other attribute
+*/
+
+function drawSketch() {
+    var sketchArea = $("canvas");
+    var sketchCtx = sketchArea[0].getContext("2d");
+
+    var off = sketchArea.offset();
+
+    var relX = (mouse.x - off.left);
+    var relY = (mouse.y - off.top);
+
+    sketchCtx.fillStyle = $("#colorPicker").val();
+    if (mouse.left) {
+        sketchCtx.fillRect(relX, relY, $("#brushSize").val(), $("#brushSize").val());
+    }
+    //console.log($("#colorPicker").val());
+    //console.log("X: " + relX + ", Y: " + relY);
+}
+
+function clearSketch() {
+    var sketchArea = $("canvas");
+    var sketchCtx = sketchArea[0].getContext("2d");
+    sketchCtx.clearRect(0, 0, sketchArea.width(), sketchArea.height());
+}
+
+var mouse = { x: 0, y: 0, left: false };
+
+document.addEventListener('mousemove', function (e) {
+    mouse.x = e.pageX || e.clientX;
+    mouse.y = e.pageY || e.clientY;
+}, false);
+
+document.addEventListener('mousedown', function (e) {
+    mouse.left = (e.button === 0);
+}, false);
+
+document.addEventListener('mouseup', function (e) {
+    mouse.left = false;
+}, false);
